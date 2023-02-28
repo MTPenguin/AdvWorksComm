24f3ada3cb34034aafd3ef58d4137402f683c45b
MTPenguin <mtpenguin@gmail.com>
2023-02-28 13:40:02 -0700

    Trigger

--- migrations/V003_20230224210757__BMac.sql
+++ migrations/V003_20230224210757__BMac.sql
@@ -15,4 +15,3 @@ UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 4
 UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 5
 UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 6
 
-
