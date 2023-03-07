061a85c720579708eadbf0d1740d5482b864583d
MTPenguin <mtpenguin@gmail.com>
2023-03-07 06:29:35 -0700

    Trigger new history

--- migrations/V001_20230224120276__baseline.sql
+++ migrations/V001_20230224120276__baseline.sql
@@ -1324,3 +1324,4 @@ EXEC sp_addextendedproperty N'MS_Description', N'Contains objects related to pro
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'AdventureWorksLT 2012 Sample OLTP Database', NULL, NULL, NULL, NULL, NULL, NULL
 GO
+
