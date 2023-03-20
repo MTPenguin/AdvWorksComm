60167c9736c9698da6ac7f9c5d494cf00805ebbe
MTPenguin <mtpenguin@gmail.com>
2023-03-20 13:01:13 -0600

    Reset base

--- migrations/V1.0.20230315151755__abc-150-data-v1.0.20230315151351.sql
+++
@@ -1,8 +0,0 @@
---flybot created V1.0.20230315151755__abc-150-data-v1.0.20230315151351
-PRINT(N'Update 6 rows in [SalesLT].[Customer]')
-UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 1
-UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 2
-UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 3
-UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 4
-UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 5
-UPDATE [SalesLT].[Customer] SET [Suffix]=NULL WHERE [CustomerID] = 6
