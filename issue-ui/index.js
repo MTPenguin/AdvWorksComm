dec723b19ad7e286f684175df997567731b3f363
MTPenguin <mtpenguin@gmail.com>
2023-03-27 06:43:35 -0600

    Polish

--- issue-ui/index.js
+++ issue-ui/index.js
@@ -1,110 +1,115 @@
 import m from 'mithril'
 
+const gui = () => {
+  /* Add JavaScript code here! */
+  console.log('Hello World! You did it! Welcome to Snowpack :D');
 
-/* Add JavaScript code here! */
-console.log('Hello World! You did it! Welcome to Snowpack :D');
-
-let JiraInput = {
-  error: '',
-  value: 'ABC-000',
-  validate: () => {
-    const regEx = /[a-zA-Z][a-zA-Z][a-zA-Z]-[0-9][0-9][0-9]/
-    const goodFormat = regEx.test(JiraInput.value)
-    JiraInput.error = !(JiraInput.value && goodFormat) ? 'Please enter Jira issue with format (XXX-000)  X = Alpha, 0 = Numeric' : '';
-  },
-  isValid: () => {
-    return JiraInput.error ? false : true;
-  },
-  view: () => {
-    return [
-      m('label', 'Jira issue'),
-      m('input', {
-        className: JiraInput.error ? 'error' : '',
-        placeholder: '(XXX-000)',
-        value: JiraInput.value,
-        type: 'text',
-        oninput: e => {
-          JiraInput.value = e.target.value;
-          JiraInput.error && JiraInput.validate()
-        }
-      }),
-      JiraInput.error && m('div.error-message', JiraInput.error)
-    ];
-  }
-};
-
-let ScopeInput = {
-  error: '',
-  value: 'data',
-  validate: () => {
-    ScopeInput.error = !ScopeInput.value ? 'Please select issue scope' : '';
-  },
-  isValid: () => {
-    return ScopeInput.error ? false : true;
-  },
-  view: () => {
-    return [
-      m('label', 'Scope'),
-      m('select', {
-        className: ScopeInput.error ? 'error' : '',
-        onchange: e => {
-          ScopeInput.value = e.target.value;
-          ScopeInput.error && ScopeInput.validate()
-        },
-        value: ScopeInput.value
-      },
-        ['data', 'refData', 'schema'].map(x =>
-          m('option', x)
-        )
-      ),
-      ScopeInput.error && m('div.error-message', ScopeInput.error)
-    ];
-  }
-};
-
-let IssueForm = {
-  isValid() {
-    JiraInput.validate();
-    ScopeInput.validate();
-    if (JiraInput.isValid() && ScopeInput.isValid()) {
-      return true;
-    }
-    return false;
-  },
-  view() {
-    return m('form', [
-      m('h1',
-        'Create Jira Issue'
-      ),
-      // Passing component
-      m(JiraInput),
-      m(ScopeInput),
-      m('button', {
-        class: 'pure-button pure-button-primary',
-        id: 'loginBtn',
-        type: 'button',
-        disabled: !(JiraInput.value && ScopeInput.value),
-        onclick() {
-          const url = "/flybot/:owner/:repo/createIssue"
-          console.log('url:', url)
-          if (IssueForm.isValid()) {
-            console.log('**** FIRE REQUEST ?:', JiraInput.value)
-            m.request({
-              method: "POST",
-              url,
-              params: { owner: 'MTPenguin', repo: 'AdvWorksComm' },
-              body: { jira: JiraInput.value, scope: ScopeInput.value }
-            })
-              .then(function (result) {
-                console.log(result)
-              })
+  let JiraInput = {
+    error: '',
+    value: 'ABC-000',
+    validate: () => {
+      const regEx = /[a-zA-Z][a-zA-Z][a-zA-Z]-[0-9][0-9][0-9]/
+      const goodFormat = regEx.test(JiraInput.value)
+      JiraInput.error = !(JiraInput.value && goodFormat) ? 'Please enter Jira issue with format (XXX-000)  X = Alpha, 0 = Numeric' : '';
+    },
+    isValid: () => {
+      return JiraInput.error ? false : true;
+    },
+    view: () => {
+      return [
+        m('label', 'Jira issue'),
+        m('input', {
+          className: JiraInput.error ? 'error' : '',
+          placeholder: '(XXX-000)',
+          value: JiraInput.value,
+          type: 'text',
+          oninput: e => {
+            JiraInput.value = e.target.value;
+            JiraInput.error && JiraInput.validate()
           }
-        }
-      },
-        'Create Issue'
-      )
-    ])
+        }),
+        JiraInput.error && m('div.error-message', JiraInput.error)
+      ];
+    }
+  };
+
+  let ScopeInput = {
+    error: '',
+    value: 'data',
+    validate: () => {
+      ScopeInput.error = !ScopeInput.value ? 'Please select issue scope' : '';
+    },
+    isValid: () => {
+      return ScopeInput.error ? false : true;
+    },
+    view: () => {
+      return [
+        m('label', 'Scope'),
+        m('select', {
+          className: ScopeInput.error ? 'error' : '',
+          onchange: e => {
+            ScopeInput.value = e.target.value;
+            ScopeInput.error && ScopeInput.validate()
+          },
+          value: ScopeInput.value
+        },
+          ['data', 'refData', 'schema'].map(x =>
+            m('option', x)
+          )
+        ),
+        ScopeInput.error && m('div.error-message', ScopeInput.error)
+      ];
+    }
+  };
+
+  let IssueForm = {
+    isValid() {
+      JiraInput.validate();
+      ScopeInput.validate();
+      if (JiraInput.isValid() && ScopeInput.isValid()) {
+        return true;
+      }
+      return false;
+    },
+    view() {
+      return m('form', [
+        m('h1',
+          'Create Jira Issue'
+        ),
+        // Passing component
+        m(JiraInput),
+        m(ScopeInput),
+        m('button', {
+          class: 'pure-button pure-button-primary',
+          id: 'loginBtn',
+          type: 'button',
+          disabled: !(JiraInput.value && ScopeInput.value),
+          onclick() {
+            const url = "/flybot/:owner/:repo/createIssue"
+            console.log('url:', url)
+            if (IssueForm.isValid()) {
+              console.log('**** FIRE REQUEST ?:', JiraInput.value)
+              m.request({
+                method: "POST",
+                url,
+                params: { owner: 'MTPenguin', repo: 'AdvWorksComm' },
+                body: { jira: JiraInput.value, scope: ScopeInput.value }
+              })
+                .then(function (result) {
+                  console.log(result)
+                })
+            }
+          }
+        },
+          'Create Issue'
+        )
+      ])
+    }
   }
+
+  m.mount(document.body, IssueForm)
 }
 
-m.mount(document.body, IssueForm)
+gui()
+
+export default gui
