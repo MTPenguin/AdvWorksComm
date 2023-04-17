3e1cc02ef69c28f1efb9b5a7f306245cc993dd88
MTPenguin <mtpenguin@gmail.com>
2023-04-17 05:43:43 -0600

    Test push

--- flybot/index.js
+++ flybot/index.js
@@ -222,199 +222,199 @@ module.exports = (app, { getRouter }) => {
   /************************************************************************************************************************************
   Event handlers
   ************************************************************************************************************************************/
-  app.on("issues.opened", async (context) => {
-    const octokit = context.octokit
-    const payload = context.payload
-    const repo = payload.repository
-    const issueBody = payload.issue.body
-    const repoOwner = repo.owner.login
-    const repoName = repo.name
+  // app.on("issues.opened", async (context) => {
+  //   const octokit = context.octokit
+  //   const payload = context.payload
+  //   const repo = payload.repository
+  //   const issueBody = payload.issue.body
+  //   const repoOwner = repo.owner.login
+  //   const repoName = repo.name
 
-    // consoleLog(thisFile
-    //   , `\npayload:`, payload
-    //   // , `\nrepo:`, repo
-    //   , `\nissueBody:`, issueBody
-    //   , `\nrepoOwner:`, repoOwner
-    //   , `\nrepoName:`, repoName
-    // )
+  //   // consoleLog(thisFile
+  //   //   , `\npayload:`, payload
+  //   //   // , `\nrepo:`, repo
+  //   //   , `\nissueBody:`, issueBody
+  //   //   , `\nrepoOwner:`, repoOwner
+  //   //   , `\nrepoName:`, repoName
+  //   // )
 
-    /**
-     * Does issue body contain valid json?
-     */
-    const firstT = issueBody.indexOf('```')
-    const lastT = issueBody.lastIndexOf('```')
-    let jsonBody
-    if (~firstT && ~lastT) {
-      try {
-        jsonBody = issueBody.substr(firstT + 3, (lastT - firstT) - 3)
-        consoleLog(thisFile, 'jsonBody:', jsonBody)
-        jsonBody = JSON.parse(jsonBody)
-      } catch (error) {
-        console.error('Throwing:', error.message)
-        throw error
-      }
-      consoleLog(thisFile, 'jsonBody:', jsonBody)
-    } else {
-      consoleLog(thisFile, 'firstT:', firstT, 'lastT:', lastT)
-      throw new Error('No JSON body')
-    }
+  //   /**
+  //    * Does issue body contain valid json?
+  //    */
+  //   const firstT = issueBody.indexOf('```')
+  //   const lastT = issueBody.lastIndexOf('```')
+  //   let jsonBody
+  //   if (~firstT && ~lastT) {
+  //     try {
+  //       jsonBody = issueBody.substr(firstT + 3, (lastT - firstT) - 3)
+  //       consoleLog(thisFile, 'jsonBody:', jsonBody)
+  //       jsonBody = JSON.parse(jsonBody)
+  //     } catch (error) {
+  //       console.error('Throwing:', error.message)
+  //       throw error
+  //     }
+  //     consoleLog(thisFile, 'jsonBody:', jsonBody)
+  //   } else {
+  //     consoleLog(thisFile, 'firstT:', firstT, 'lastT:', lastT)
+  //     throw new Error('No JSON body')
+  //   }
 
-    if (!(jsonBody.jira && jsonBody.scope)) throw new Error('Missing parameter(s) jsonBody.jira && jsonBody.scope ' + JSON.stringify(jsonBody))
+  //   if (!(jsonBody.jira && jsonBody.scope)) throw new Error('Missing parameter(s) jsonBody.jira && jsonBody.scope ' + JSON.stringify(jsonBody))
 
-    /**
-     * Get current version
-     */
-    const tags = await octokit.request(repo.tags_url)
-    consoleLog(thisFile, 'tags:', tags)
-    const tagsSorted = semver.rsort(tags.data.map(d => d.name))
-    consoleLog(thisFile, 'tagsSorted:', tagsSorted)
-    const currentVersion = tagsSorted[0]
-    consoleLog(thisFile, 'currentVersion:', currentVersion)
+  //   /**
+  //    * Get current version
+  //    */
+  //   const tags = await octokit.request(repo.tags_url)
+  //   consoleLog(thisFile, 'tags:', tags)
+  //   const tagsSorted = semver.rsort(tags.data.map(d => d.name))
+  //   consoleLog(thisFile, 'tagsSorted:', tagsSorted)
+  //   const currentVersion = tagsSorted[0]
+  //   consoleLog(thisFile, 'currentVersion:', currentVersion)
 
-    /**
-    * Flyway Date Stamp yyyy-MM-dd HH:mm:ss
-    */
-    const dateNow = new Date()
-    const dateStamp = dateNow.getUTCFullYear() + String(dateNow.getUTCMonth() + 1).padStart(2, '0') + String(dateNow.getUTCDate()).padStart(2, '0')
-      + String(dateNow.getUTCHours()).padStart(2, '0') + String(dateNow.getUTCMinutes()).padStart(2, '0') + String(dateNow.getUTCSeconds()).padStart(2, '0')
-    consoleLog(thisFile, 'dateStamp:', dateStamp)
+  //   /**
+  //   * Flyway Date Stamp yyyy-MM-dd HH:mm:ss
+  //   */
+  //   const dateNow = new Date()
+  //   const dateStamp = dateNow.getUTCFullYear() + String(dateNow.getUTCMonth() + 1).padStart(2, '0') + String(dateNow.getUTCDate()).padStart(2, '0')
+  //     + String(dateNow.getUTCHours()).padStart(2, '0') + String(dateNow.getUTCMinutes()).padStart(2, '0') + String(dateNow.getUTCSeconds()).padStart(2, '0')
+  //   consoleLog(thisFile, 'dateStamp:', dateStamp)
 
-    let level
-    let newVersion
-    const currentMajor = semver.major(currentVersion)
-    const currentMinor = semver.minor(currentVersion)
-    switch (jsonBody.scope) {
-      case 'data':
-        level = 'patch'
-        newVersion = currentMajor + '.' + currentMinor + '.' + dateStamp
-        break
-      case 'refData':
-        level = 'minor'
-        newVersion = semver.inc(currentVersion, level)
-        break
-      case 'schema':
-        level = 'major'
-        newVersion = semver.inc(currentVersion, level)
-    }
-    consoleLog(thisFile, 'level:', level)
-    consoleLog(thisFile, 'newVersion:', newVersion)
+  //   let level
+  //   let newVersion
+  //   const currentMajor = semver.major(currentVersion)
+  //   const currentMinor = semver.minor(currentVersion)
+  //   switch (jsonBody.scope) {
+  //     case 'data':
+  //       level = 'patch'
+  //       newVersion = currentMajor + '.' + currentMinor + '.' + dateStamp
+  //       break
+  //     case 'refData':
+  //       level = 'minor'
+  //       newVersion = semver.inc(currentVersion, level)
+  //       break
+  //     case 'schema':
+  //       level = 'major'
+  //       newVersion = semver.inc(currentVersion, level)
+  //   }
+  //   consoleLog(thisFile, 'level:', level)
+  //   consoleLog(thisFile, 'newVersion:', newVersion)
 
-    /**
-     * In order to create a new branch off of default, we first have to get the sha of mergeBranch
-     */
-    const mergeBranch = await octokit.request(repo.branches_url, { branch: repo.default_branch })
-    consoleLog(thisFile, 'mergeBranch:', mergeBranch)
+  //   /**
+  //    * In order to create a new branch off of default, we first have to get the sha of mergeBranch
+  //    */
+  //   const mergeBranch = await octokit.request(repo.branches_url, { branch: repo.default_branch })
+  //   consoleLog(thisFile, 'mergeBranch:', mergeBranch)
 
-    /* DEBUG ADD LIGHT TAG REF */
-    // await octokit.git.createRef({
-    //   owner: repoOwner,
-    //   repo: repoName,
-    //   ref: 'refs/tags/v1.0.' + dateStamp,
-    //   sha: mergeBranch.data.commit.sha
-    // })
-
-
-    // GITHUB_ISSUE-JIR-000-SCOPE-CURRENT_VERSION
-    const newBranch = payload.issue.number + '-' + jsonBody.jira + '-' + jsonBody.scope + '-' + currentVersion
-    const newMigration = 'V' + newVersion + '__' + newBranch
-    consoleLog(thisFile, 'newBranch:', newBranch)
-    consoleLog(thisFile, 'newMigration:', newMigration)
+  //   /* DEBUG ADD LIGHT TAG REF */
+  //   // await octokit.git.createRef({
+  //   //   owner: repoOwner,
+  //   //   repo: repoName,
+  //   //   ref: 'refs/tags/v1.0.' + dateStamp,
+  //   //   sha: mergeBranch.data.commit.sha
+  //   // })
 
 
-    /**
-     * Create a new branch
-     */
-    let result = await octokit.git.createRef({
-      owner: repoOwner,
-      repo: repoName,
-      ref: `refs/heads/${newBranch}`,
-      sha: mergeBranch.data.commit.sha,
-      key: payload.issue.number
-    });
-    consoleLog(thisFile, 'branch result:', result)
+  //   // GITHUB_ISSUE-JIR-000-SCOPE-CURRENT_VERSION
+  //   const newBranch = payload.issue.number + '-' + jsonBody.jira + '-' + jsonBody.scope + '-' + currentVersion
+  //   const newMigration = 'V' + newVersion + '__' + newBranch
+  //   consoleLog(thisFile, 'newBranch:', newBranch)
+  //   consoleLog(thisFile, 'newMigration:', newMigration)
 
 
-    /*
-    * [ Resolves #<issue_number> ] links the commit to the issue.  When the commit is merged, it should close the issue.
-    * TODO Trying to get the linked branch to show up under 'Development' in the GitHub Issue UI
-    */
-    const DEBUG = jsonBody.debug ?? false
-    let message = `Resolves #${payload.issue.number} - Created ${newMigration}.sql file - [skip actions]}`
-    let content = "--flybot created " + newMigration
-    if (DEBUG) {
-      content += "\n-- DEBUG ---\n"
-      const debugVal = dateStamp.substring(dateStamp.length - 10, 10)
-      content += `PRINT(N'Update 6 rows in [SalesLT].[Customer]')
-      UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 1
-      UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 2
-      UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 3
-      UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 4
-      UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 5
-      UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 6
-    `
-      content += "-- DEBUG ---\n\n\n"
-    }
+  //   /**
+  //    * Create a new branch
+  //    */
+  //   let result = await octokit.git.createRef({
+  //     owner: repoOwner,
+  //     repo: repoName,
+  //     ref: `refs/heads/${newBranch}`,
+  //     sha: mergeBranch.data.commit.sha,
+  //     key: payload.issue.number
+  //   });
+  //   consoleLog(thisFile, 'branch result:', result)
 
-    result = await octokit.repos.createOrUpdateFileContents({
-      owner: repoOwner,
-      repo: repoName,
-      branch: newBranch,
-      path: 'migrations/' + newMigration + '.sql',
-      message,
-      content: encode(content)
-    })
-    consoleLog(thisFile, 'migration file result:', result)
 
-    content = {
-      ...jsonBody,
-      newBranch,
-      currentVersion,
-      currentMajor,
-      currentMinor,
-      dateStamp,
-      newVersion
-    }
-    consoleLog(thisFile, 'content:', content)
+  //   /*
+  //   * [ Resolves #<issue_number> ] links the commit to the issue.  When the commit is merged, it should close the issue.
+  //   * TODO Trying to get the linked branch to show up under 'Development' in the GitHub Issue UI
+  //   */
+  //   const DEBUG = jsonBody.debug ?? false
+  //   let message = `Resolves #${payload.issue.number} - Created ${newMigration}.sql file - [skip actions]}`
+  //   let content = "--flybot created " + newMigration
+  //   if (DEBUG) {
+  //     content += "\n-- DEBUG ---\n"
+  //     const debugVal = dateStamp.substring(dateStamp.length - 10, 10)
+  //     content += `PRINT(N'Update 6 rows in [SalesLT].[Customer]')
+  //     UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 1
+  //     UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 2
+  //     UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 3
+  //     UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 4
+  //     UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 5
+  //     UPDATE [SalesLT].[Customer] SET [Suffix]='${debugVal}' WHERE [CustomerID] = 6
+  //   `
+  //     content += "-- DEBUG ---\n\n\n"
+  //   }
 
-    /**
-    * Create a new issue comment
-    */
-    let commentBody = `Thank you ${payload.issue.user.login} for creating issue #${payload.issue.number}, Jira:[${jsonBody.jira}](${process.env.JIRA_BROWSE_URL}/${jsonBody.jira})!\n\n\n`
-    commentBody += "A new branch (["
-    commentBody += newBranch
-    commentBody += "](https://github.com/MTPenguin/AdvWorksComm/tree/"
-    commentBody += newBranch
-    commentBody += ")) has been created for this migration."
-    const issueComment = context.issue({
-      body: commentBody,
-    });
-    result = await octokit.issues.createComment(issueComment);
-    consoleLog(thisFile, 'issue comment result:', result)
+  //   result = await octokit.repos.createOrUpdateFileContents({
+  //     owner: repoOwner,
+  //     repo: repoName,
+  //     branch: newBranch,
+  //     path: 'migrations/' + newMigration + '.sql',
+  //     message,
+  //     content: encode(content)
+  //   })
+  //   consoleLog(thisFile, 'migration file result:', result)
 
-    /**
-    * Update issue JSON
-    */
-    const body = `\`\`\`${JSON.stringify(content)}\`\`\``
-    consoleLog(thisFile, 'body:', body)
-    result = await octokit.issues.update({
-      owner: repoOwner,
-      repo: repoName,
-      issue_number: payload.issue.number,
-      title: newBranch,
-      body,
-      // state: 'open',
-      labels: [jsonBody.jira, jsonBody.scope],
-      // DEBUG try to link branch
-      // development: {
-      //   branch: newBranch
-      // }
-    });
-    consoleLog(thisFile, 'issue update result:', result)
-  });
+  //   content = {
+  //     ...jsonBody,
+  //     newBranch,
+  //     currentVersion,
+  //     currentMajor,
+  //     currentMinor,
+  //     dateStamp,
+  //     newVersion
+  //   }
+  //   consoleLog(thisFile, 'content:', content)
 
-  app.on('push', async (context) => {
-    consoleLog(thisFile, 'Push event context:', context)
-  })
+  //   /**
+  //   * Create a new issue comment
+  //   */
+  //   let commentBody = `Thank you ${payload.issue.user.login} for creating issue #${payload.issue.number}, Jira:[${jsonBody.jira}](${process.env.JIRA_BROWSE_URL}/${jsonBody.jira})!\n\n\n`
+  //   commentBody += "A new branch (["
+  //   commentBody += newBranch
+  //   commentBody += "](https://github.com/MTPenguin/AdvWorksComm/tree/"
+  //   commentBody += newBranch
+  //   commentBody += ")) has been created for this migration."
+  //   const issueComment = context.issue({
+  //     body: commentBody,
+  //   });
+  //   result = await octokit.issues.createComment(issueComment);
+  //   consoleLog(thisFile, 'issue comment result:', result)
+
+  //   /**
+  //   * Update issue JSON
+  //   */
+  //   const body = `\`\`\`${JSON.stringify(content)}\`\`\``
+  //   consoleLog(thisFile, 'body:', body)
+  //   result = await octokit.issues.update({
+  //     owner: repoOwner,
+  //     repo: repoName,
+  //     issue_number: payload.issue.number,
+  //     title: newBranch,
+  //     body,
+  //     // state: 'open',
+  //     labels: [jsonBody.jira, jsonBody.scope],
+  //     // DEBUG try to link branch
+  //     // development: {
+  //     //   branch: newBranch
+  //     // }
+  //   });
+  //   consoleLog(thisFile, 'issue update result:', result)
+  // });
+
+  // app.on('push', async (context) => {
+  //   consoleLog(thisFile, 'Push event context:', context)
+  // })
 
   app.onAny(async (context) => {
     consoleLog(thisFile, 'onAny:', { event: context.name, action: context.payload.action });
