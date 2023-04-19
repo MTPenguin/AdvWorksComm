23b1e38199e0dea3198bdad48d8ec94375833c84
MTPenguin <mtpenguin@gmail.com>
2023-04-19 11:48:12 -0600

    Debug cmdLn

--- flybot/index.js
+++ flybot/index.js
@@ -598,13 +598,8 @@ module.exports = (app, { getRouter }) => {
     const commits = context.payload.commits
     const { $ } = await import('execa')
 
-    // const cmdLn = exec => async options => await $`${'flyway -community -user=' + process.env.DB_USERNAME + ' -password=' + process.env.DB_PASSWORD + ' -configFiles=../flyway.conf -locations=filesystem:../migrations'} info -url=${process.env.DB_JDBC} -outputType=json`
-    // const cmdLn = exec => async options => await $`${'flyway'} ${`-community -user=${process.env.DB_USERNAME}`} -password=${process.env.DB_PASSWORD} -configFiles=../flyway.conf -locations=filesystem:../migrations info -url=${process.env.DB_JDBC} -outputType=json`
-    // const cmdLn = exec => async options => {
-    //   const built = `flyway -community -user=${process.env.DB_USERNAME} -password=${process.env.DB_PASSWORD} -configFiles=../flyway.conf -locations=filesystem:../migrations info -url=${process.env.DB_JDBC} -outputType=json`
-    //   await $`${built}`
-    // }
-    const cmdLn = exec => async options => exec`flyway -community -user=${process.env.DB_USERNAME} -password=${process.env.DB_PASSWORD} -configFiles=../flyway.conf -locations=filesystem:../migrations ${options.cmds} -url=${process.env.DB_JDBC} -outputType=json`
+    const fwCmdLn = async cmds => $`flyway -community -user=${process.env.DB_USERNAME} -password=${process.env.DB_PASSWORD} -configFiles=../flyway.conf -locations=filesystem:../migrations ${cmds} -url=${process.env.DB_JDBC} -outputType=json`
+
     const branch = context.payload.ref.substring(String('refs/heads/').length)
     DEBUG && consoleLog(thisFile, 'branch:', branch)
 
@@ -638,7 +633,7 @@ module.exports = (app, { getRouter }) => {
           // const result = await $`flyway -community -user=${process.env.DB_USERNAME} -password=${process.env.DB_PASSWORD} -configFiles=../flyway.conf -locations=filesystem:../migrations info -url=${process.env.DB_JDBC} -outputType=json`
           // flyway -community -user=sa -password=saPass11 -configFiles=../flyway.conf -locations=filesystem:../migrations info -url=jdbc:sqlserver://10.211.55.2;authentication=sqlPassword;databaseName=AdvWorksComm;encrypt=true;integratedSecurity=false;trustServerCertificate=true -outputType=json
 
-          const result = await cmdLn($)({ pre: `flyway -community -user=${process.env.DB_USERNAME} -password=${process.env.DB_PASSWORD} -configFiles=../flyway.conf -locations=filesystem:../migrations`, post: `-url=${process.env.DB_JDBC} -outputType=json`, cmds: 'info' })
+          const result = await fwCmdLn('info')
           // flyway -community -user=sa -password=saPass11 -configFiles=../flyway.conf -locations=filesystem:../migrations info -url=jdbc:sqlserver://10.211.55.2;authentication=sqlPassword;databaseName=AdvWorksComm;encrypt=true;integratedSecurity=false;trustServerCertificate=true -outputType=json
           // flyway -community -user=sa -password=saPass11 -configFiles=../flyway.conf -locations=filesystem:../migrations info -url=jdbc:sqlserver://10.211.55.2;authentication=sqlPassword;databaseName=AdvWorksComm;encrypt=true;integratedSecurity=false;trustServerCertificate=true -outputType=json
           DEBUG && consoleLog(thisFile, 'result:', result);
