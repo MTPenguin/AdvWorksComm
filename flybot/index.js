e00c3310aeb15872dfaa8bbb58e251d8c2d7014e
MTPenguin <mtpenguin@gmail.com>
2023-04-19 08:54:24 -0600

    Debug execa

--- flybot/index.js
+++ flybot/index.js
@@ -626,7 +626,7 @@ module.exports = (app, { getRouter }) => {
         const fwCmd = `ls -la` // > ../reports/${branch}.json`
 
         try {
-          const { stdout, stderr } = await $`${fwCmd}`
+          const stdout = await $`${fwCmd}`
           DEBUG && consoleLog(thisFile, 'stdout:', stdout);
           DEBUG && stderr && console.error(thisFile, 'stderr:', stderr);
           const info = JSON.parse(stdout)
