a5e4d68ec460b5f2f2c185d1bde5fa4cbcdac7fd
MTPenguin <mtpenguin@gmail.com>
2023-04-19 12:21:44 -0600

    Debug build

--- flybot/index.js
+++ flybot/index.js
@@ -636,7 +636,7 @@ module.exports = (app, { getRouter }) => {
             const cleanResult = await fwCmdLn(process.env.DB_BUILD_JDBC)('clean')
             DEBUG && consoleLog(thisFile, 'cleanResult:', cleanResult);
             const cleanJson = JSON.parse(cleanResult.stdout)
-            const buildResult = await fwCmdLn(process.env.DB_BUILD_JDBC)('build')
+            const buildResult = await fwCmdLn(process.env.DB_BUILD_JDBC)('migrate')
             DEBUG && consoleLog(thisFile, 'buildResult:', buildResult);
             const buildJson = JSON.parse(buildResult.stdout)
 
